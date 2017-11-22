230c5668b00688fe63f2d628f36db1698f98c1dc
Clark Zhang <clarkz@seas.upenn.edu>
2017-11-22 17:53:56 -0500

    fixed rrt_bi and est bugs

--- est_env.py
+++ est_env.py
@@ -16,7 +16,7 @@ class ESTEnv(object):
         self.map_info = map_info
         self.state_size = len(self.map_info['start'])
 
-        self.r = 3 # radius of sampling
+        self.r = 4. # radius of sampling
         self.n = 1 # number of samples to take each iteration
         self.reset()
 
@@ -28,24 +28,25 @@ class ESTEnv(object):
         node_idx = np.where(dist_mask)[0]
         return node_idx
 
-    def _compute_and_update_w(self, node, neighbor_idx=None):
-        if np.any(neighbor_idx == None):
-            neighbor_idx = self.radius_search(self.tree.node_states, node, self.r)
-        self.tree.node_info[-1] = 1.0 / (len(neighbor_idx) + 1)
-        self.total_weight += self.tree.node_info[-1]
+    def _compute_and_update_w(self, node, neighbor_idx, tree_idx):
+        tree = self.trees[tree_idx]
+
+        tree.node_info[-1] = 1.0 / (len(neighbor_idx) + 1)
+        self.total_weights[tree_idx] += tree.node_info[-1]
 
         for idx in neighbor_idx:
-            w = self.tree.node_info[idx]
-            self.tree.node_info[idx] = w / (w + 1.)
-            self.total_weight += (self.tree.node_info[idx] - w)
+            w = tree.node_info[idx]
+            tree.node_info[idx] = w / (w + 1.)
+            self.total_weights[tree_idx] += (tree.node_info[idx] - w)
 
     def _sample_node(self):
         sample = np.random.random_sample()
 
         weight_sum = 0
         idx = None
-        for i in range(len(self.tree.node_info)):
-            weight_sum += self.tree.node_info[i] / self.total_weight
+        tree = self.trees[self.tree_idx]
+        for i in range(len(tree.node_info)):
+            weight_sum += tree.node_info[i] / self.total_weights[self.tree_idx]
             if weight_sum > sample:
                 idx = i
                 break
@@ -55,7 +56,7 @@ class ESTEnv(object):
     def sample_free_space(self, node, r):
         # with eps chance, sample goal
         rand = np.random.random_sample()
-        if rand < 0.1:
+        if rand < 0.05:
             goal = self.map_info['goal']
             direction = goal - node
             dir_len = np.linalg.norm(direction)
@@ -63,7 +64,7 @@ class ESTEnv(object):
                 sample = node + (r * direction / dir_len)
                 if not self.config['collision_check'](self.map_info['map'], sample):
                     return sample
-                    
+
         sample = node + sample_ball(r)
         while self.config['collision_check'](self.map_info['map'], sample):
             sample = node + sample_ball(r)
@@ -71,31 +72,46 @@ class ESTEnv(object):
 
 
     def reset(self):
-        self.tree = Tree()
+
+        self.forward_tree = Tree()
+
+        # node info is 1/w(x)
+        self.forward_tree.insert_node(self.map_info['start'], 1)
+        # keeps track of sum of 1/w(x)
+        # self.total_weight = 1
+
+
+        self.backward_tree = Tree()
+        self.backward_tree.insert_node(self.map_info['goal'], 1)
+        self.trees = [self.forward_tree, self.backward_tree]
+        self.total_weights = [1., 1.]
+        self.tree_idx = 0
+
+
+        # self.tree = Tree()
         self.found_path = False
         self.num_collision_checks = 0
 
-        # node info is 1/w(x)
-        self.tree.insert_node(self.map_info['start'], 1)
-        # keeps track of sum of 1/w(x)
-        self.total_weight = 1
 
         # self.rand_node = self.config['random_sample'](self.map_info)
         self.rand_node_idx = self._sample_node()
-        self.rand_node = self.tree.node_states[self.rand_node_idx]
+        self.rand_node = self.trees[self.tree_idx].node_states[self.rand_node_idx]
         self.node_feat = self.config['feat'](self.rand_node, 
-            self.tree, 
+            self.trees, 
             self.map_info)
 
         return self.node_feat
 
     def __run(self):
+        tree = self.trees[self.tree_idx]
+        other_tree = self.trees[1 - self.tree_idx]
+
         samples = []
         for i in range(self.n):
             samples.append(self.sample_free_space(self.rand_node, self.r))
 
         for sample in samples:
-            neighbor_idx = self._radius_search(self.tree.node_states, sample, self.r)
+            neighbor_idx = self._radius_search(tree.node_states, sample, self.r)
             w = len(neighbor_idx) + 1
             if np.random.sample() > 1.0 / w:
                 continue
@@ -108,10 +124,36 @@ class ESTEnv(object):
                 continue
 
             new_node = path[-1]
-            self.tree.insert_node(new_node, node_info=1, parent_idx=self.rand_node_idx)
-            self._compute_and_update_w(new_node, neighbor_idx)
+            tree.insert_node(new_node, node_info=1, parent_idx=self.rand_node_idx)
+            self._compute_and_update_w(new_node, neighbor_idx, self.tree_idx)
 
-            if self.config['goal_region'](new_node, self.map_info['goal']):
+
+            # check if forward tree has reached
+            if self.tree_idx == 0 and self.config['goal_region'](new_node, self.map_info['goal']):
+                self.found_path = True
+                break
+
+            # check if backward tree has reached
+            if self.tree_idx == 1 and np.linalg.norm(new_node - self.map_info['start']) < 1e-1:
+                self.found_path = True
+                break
+
+            # try to link trees
+            closest_idx = other_tree.closest_idx(new_node, self.config['dist'])
+            closest_node = other_tree.node_states[closest_idx]
+
+            path, path_cost = self.config['steer'](closest_node, new_node)
+            collision, num_checks = self.config['collision_check'](self.map_info['map'], path, True)
+            self.num_collision_checks += num_checks
+            if collision:
+                continue
+            new_other_node = path[-1]
+            neighbor_idx = self._radius_search(other_tree.node_states, new_other_node, self.r)
+            other_tree.insert_node(new_other_node, node_info=1, parent_idx=closest_idx)
+            self._compute_and_update_w(new_other_node, neighbor_idx, 1-self.tree_idx)
+
+
+            if np.linalg.norm(new_node - new_other_node) < 1e-1:
                 self.found_path = True
                 break
 
@@ -122,19 +164,22 @@ class ESTEnv(object):
 
 
         prev_num_coll_checks = self.num_collision_checks
-        prev_node_states = len(self.tree.node_states)
+        prev_node_states = len(self.forward_tree.node_states) + len(self.backward_tree.node_states)
         if action == 1:
             self.__run()
+        self.tree_idx = 1 - self.tree_idx
 
+
+        new_node_states = len(self.forward_tree.node_states) + len(self.backward_tree.node_states)
 
         self.rand_node_idx = self._sample_node()
-        self.rand_node = self.tree.node_states[self.rand_node_idx]
+        self.rand_node = self.trees[self.tree_idx].node_states[self.rand_node_idx]
         self.node_feat = self.config['feat'](self.rand_node, 
-            self.tree, self.map_info)
+            self.trees, self.map_info)
 
         reward = 0
         reward += -0.01
-        reward += -(len(self.tree.node_states) - prev_node_states)
+        reward += -(new_node_states - prev_node_states)
         reward += -(self.num_collision_checks - prev_num_coll_checks)
 
         return self.node_feat, reward, self.found_path, None
@@ -142,10 +187,11 @@ class ESTEnv(object):
     def show(self):
         plt.cla()
         if self.found_path:
-            self.tree.show(im=self.map_info['map'], goal=self.map_info['goal'], path_idx=len(self.tree.node_states)-1)
+            self.trees[0].show(im=self.map_info['map'], path_idx=len(self.trees[0].node_states)-1)
+            self.trees[1].show(goal=self.map_info['goal'], path_idx=len(self.trees[1].node_states)-1)
         else:
-            self.tree.show(im=self.map_info['map'], goal=self.map_info['goal'])
-
+            self.trees[0].show(im=self.map_info['map'])
+            self.trees[1].show(goal=self.map_info['goal'])
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
@@ -156,19 +202,19 @@ if __name__ == '__main__':
     from tqdm import tqdm
     import time
 
-    l2_data_dict = generate_data('empty', dubins=False)
+    np.random.seed(0)
+    l2_data_dict = generate_data('fly_trap_fixed_a', dubins=False)
     l2_random_sampler = partial(map_sampler_goal_bias, eps=0.1)
     l2_goal = l2_goal_region
     l2_config = {'collision_check': map_collision_check,
               'random_sample': l2_random_sampler,
-              'steer': holonomic_steer,
+              'steer': partial(holonomic_steer, extend_length=5.),
               'dist': l2_dist,
               'goal_region': l2_goal,
-              'feat': get_feat_flytrap,
+              'feat': get_feat_default,
               'num_feat': 1,
               'precomputed': map_obst_precompute(l2_data_dict['map'])}
 
-    np.random.seed(0)
     rrt = ESTEnv(l2_config, l2_data_dict)
     # policy = BallTreePolicy()
     # policy = DynamicDomainPolicy()
@@ -185,11 +231,15 @@ if __name__ == '__main__':
         
         idx += 1
 
-        if idx % 10 == 0:
+        if idx % 8000 == 0:
             rrt.show()
             plt.show(block=False)
             plt.pause(0.1)
 
+        if done:
+            break
+
+    print("done")
     rrt.show()
     plt.show()
 
--- rrt_bi_env.py
+++ rrt_bi_env.py
@@ -37,7 +37,6 @@ class RRTBiEnv(object):
         return self.node_feat
 
     def __run(self, rand_node):
-
         curr_tree = self.trees[self.tree_idx]
         other_tree = self.trees[1 - self.tree_idx]
         env = self.map_info['map']
@@ -56,6 +55,7 @@ class RRTBiEnv(object):
             if self.tree_idx == 0 and self.config['goal_region'](new_node, self.map_info['goal']):
                 self.found_path = True
                 self.goal_idx[0] = len(curr_tree.node_states)-1
+
                 return
 
             # connect
@@ -74,9 +74,9 @@ class RRTBiEnv(object):
                 self.goal_idx[self.tree_idx] = len(curr_tree.node_states) - 1
 
                 self.found_path = True
-                return
             elif state == ExtendState.TRAPPED:
                 self.num_collisions += 1
+
 
     def step(self, action):
         if self.found_path:
@@ -87,6 +87,7 @@ class RRTBiEnv(object):
 
         if action == 1:
             self.__run(self.rand_node)
+        self.tree_idx = 1 - self.tree_idx
 
         new_node_states = len(self.forward_tree.node_states) + len(self.backward_tree.node_states)
 
@@ -151,59 +152,43 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
     from generate_data import generate_data
     from functools import partial
-    from rrt_utils import *
-    from map_utils import *
-    from gazebo_map import *
-    from CylinderRRT import * 
-    from arm_interface_ros import *
+    from utils import *
+    from policy import *
     from tqdm import tqdm
     import time
 
-    arm_map = CylinderMap('table')
-    arm_interface = ArmInterface()
-    arm_data_dict = {'map': arm_map,
-        'start': np.array([math.pi/2,
-            (260./180) * math.pi,
-            (160./180.) * math.pi, 
-            0., 0, 0.0]),
-        'goal': np.array([math.pi/2,
-            (210./180) * math.pi,
-            (120./180.) * math.pi, 
-            0., 0, 0.0])}
-    arm_helper = CylinderRRT(arm_map, arm_interface)
-    arm_config = {'collision_check': arm_helper.get_collision_check(),
-        'random_sample': arm_helper.get_random_sample(eps=0.1),
-        'steer': arm_helper.get_steer(),
-        'dist': arm_helper.get_dist_func(),
-        'goal_region': arm_goal_region,
-        'feat': arm_helper.get_feat(),
-        'num_feat': 5}
+    np.random.seed(0)
+    l2_data_dict = generate_data('fly_trap_fixed_a', dubins=False)
+    l2_random_sampler = partial(map_sampler_goal_bias, eps=0.1)
+    l2_goal = l2_goal_region
+    l2_config = {'collision_check': map_collision_check,
+              'random_sample': l2_random_sampler,
+              'steer': holonomic_steer,
+              'dist': l2_dist,
+              'goal_region': l2_goal,
+              'feat': get_feat_flytrap_bi,
+              'num_feat': 1,
+              'precomputed': map_obst_precompute(l2_data_dict['map'])}
 
 
-    rrt = RRTBiEnv(arm_config, arm_data_dict)
-    rrtpolicy = RRTPolicy(arm_config['num_feat'])
+    rrt = RRTBiEnv(l2_config, l2_data_dict)
+    policy = DefaultPolicy()
 
     obs = rrt.reset()
     done = False
 
+
+    idx = 0
     while not done:
-        action = rrtpolicy.get_action(obs)
-
+        action = policy.get_action(obs)
         obs, reward, done, _ = rrt.step(action)
+        
+        idx += 1
 
+        if idx % 4000 == 0:
+            rrt.show()
+            plt.show(block=False)
+            plt.pause(0.1)
 
-    print "Done"
-    if rrt.found_path:
-        print('Found Path')
-        num_nodes = len(rrt.forward_tree.node_states) + len(rrt.backward_tree.node_states)
-        print('Expanded ' + str(num_nodes) + ' nodes')
-        path = rrt.get_path()
-        print('Path has ' + str(len(path)) + ' nodes')
-
-
-        for joints in path:
-            print "joints:", joints
-            # print arm_helper.collision_check(arm_map, joints, debug=True)
-            _ = raw_input("Press Enter:")
-            arm_interface.send_joint_list(joints)
-
+    rrt.show()
+    plt.show()
