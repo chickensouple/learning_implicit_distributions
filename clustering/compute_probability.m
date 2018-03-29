92196cdc66f455bf38772b86996ee4b0c667d963
Clark <clarkz@seas.upenn.edu>
2018-03-29 17:36:49 -0400

    deleted unncessary directories

--- clustering/compute_probability.m
+++
@@ -1,9 +0,0 @@
-
-function prob = compute_probability(data, Means,Sigmas,Theta)
-
-for i = 1:size(Means,1)
-Y(:,i) = mvnpdf(data,Means(i,:),Sigmas(:,:,i));
-end
-prob = Theta*Y';
-end
-
--- ros/src/arm_motion_planner/src/kinematic/transforms/Transform_translateY.m
+++
@@ -1,5 +0,0 @@
-function t = Transform_translateY(t, y)
-
-  t(1,4) = t(1,4) + t(1,2)*y;
-  t(2,4) = t(2,4) + t(2,2)*y;
-  t(3,4) = t(3,4) + t(3,2)*y;
